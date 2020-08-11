0d90fb8772b57cd1d6f47e951af1c04a3b475834
Caio <caio.student@outlook.com>
2020-08-11 09:42:23 -0300

    inserindo dados no graphic

--- js/script.js
+++ js/script.js
@@ -1,52 +1,54 @@
+var ctx = document.getElementById('myChart').getContext('2d');
 // criar o grafico.
 
 var array = [];
 
 var value = 1;
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+function graphic(data) {
+  
+  var chart = new Chart(ctx, {
+      type: 'line',
 
-    data: {
-        labels: ['02/2020', '03/2020', '04/2020', '05/2020', '06/2020', '07/2020', '08/2020'],
-        datasets: [{
-            label: 'Dollar',
-            backgroundColor: 'transparent',
-            borderColor: 'blue',
-            data: array,
-            borderColor: '#f1f1f1',
-            borderWidth: 4
-        }]
-    },
-    options: {
-        scales: {
-          yAxes: [{
-              ticks: {
-                  fontColor: '#fff',
-                  zeroLineWidth: 1
-              }
+      data: {
+          labels: ['02/2020', '03/2020', '04/2020', '05/2020', '06/2020', '07/2020', '08/2020'],
+          datasets: [{
+              label: 'Dollar',
+              backgroundColor: 'transparent',
+              borderColor: 'blue',
+              data: data,
+              borderColor: '#f1f1f1',
+              borderWidth: 4
           }]
       },
-    },
+      options: {
+          scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: '#fff',
+                    zeroLineWidth: 1
+                }
+            }]
+        },
+      },
 
-    legend: {
+      legend: {
 
-      labels: {
-        fontColor: "#eee",
-        fontSize: 30,
-        fontStyle: "bold",
-        borderColor: '#f1f1f1'
+        labels: {
+          fontColor: "#eee",
+          fontSize: 30,
+          fontStyle: "bold",
+          borderColor: '#f1f1f1'
 
-      } 
-    },
+        } 
+      },
 
-  });
+    });
+  }
 
   setInterval( ()=> {
     array.push(value);
-    chart.dataset
+    graphic(array);
     value +=1;
     console.log(array)
 
