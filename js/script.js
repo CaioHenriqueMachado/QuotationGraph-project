7b1a2240b4a90ae02e16b05f86c8392a95dbe233
Caio <caio.student@outlook.com>
2020-08-14 00:41:41 -0300

    testes

--- js/script.js
+++ js/script.js
@@ -15,23 +15,23 @@ function graphic(data, label) {
               data: data,
               // data: [5.4272, 5.3825, 5.4852, 5.4378, 5.4374, 5.3291, 5.2902, 5.289, 5.3205, 5.2215, 5.222, 5.155, 5.17, 5.1627, 5.149],
               borderColor: '#f1f1f1',
-              borderWidth: 4,
-              tension: 0.3,
-              radius: 1.7,
-              border: 1.5,
-              angle:0,
+              // borderWidth: 4,
+              // tension: 0.3,
+              // radius: 1.7,
+              // border: 1.5,
+              // angle:0,
           }]
       },
-      options: {
-          scales: {
-            yAxes: [{
-                ticks: {
-                    fontColor: '#fff',
-                    zeroLineWidth: 1
-                }
-            }]
-        },
-      },
+      // options: {
+      //     scales: {
+      //       yAxes: [{
+      //           ticks: {
+      //               fontColor: '#fff',
+      //               zeroLineWidth: 1
+      //           }
+      //       }]
+      //   },
+      // },
 
       legend: {
 
@@ -51,13 +51,13 @@ function graphic(data, label) {
 
 // listar a API.
 var burl = "https://economia.awesomeapi.com.br";
-var query = "/json/daily/USD-BRL/15";
+var query = "/json/daily/USD-BRL/20";
 var results = [];
-var label = 0;
-var ttt = []
-ttt.push(1, 5.3825, 777, 5.4378, 5.4374, 5.3291, 5.2902, 5.289, 5.3205, 5.2215, 5.222, 5.155, 5.17, 5.1627, 5.149);
-console.log(ttt);
-console.log(ttt[2]);
+var label = [];
+// var ttt = []
+// ttt.push(1, 5.3825, 777, 5.4378, 5.4374, 5.3291, 5.2902, 5.289, 5.3205, 5.2215, 5.222, 5.155, 5.17, 5.1627, 5.149);
+// console.log(ttt);
+// console.log(ttt[2]);
 
 async function apiCash() {
 
@@ -67,14 +67,13 @@ await fetch(url)
 .then(response => { response.json()
  .then((data) => {
        data.forEach(element => {
-      //  console.log(element.bid);
-        label = Number(element.bid);
-        results.push(label);
+        results.push(Number(element.bid));
+        label.push('');
 
-     
-   });
-  console.log(results);
-   
+  });
+
+  graphic(results, label);
+
  }).catch(e => console.log(e))
 
 }).catch(e => console.log(e))
@@ -83,19 +82,8 @@ await fetch(url)
 
 
 
-// function getValue(value) {
-//   results.push(value);
-// }
-
-
-
-
-
-
-
-console.log(results)
 apiCash();
-graphic(results, label);
+
 
   // setInterval( ()=> {
   //   // console.log(apiCash())
